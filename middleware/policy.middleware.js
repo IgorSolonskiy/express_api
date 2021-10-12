0a3fac8d7ac060d72551bb4ad5304b7f37951e70
solonskiy <igorsolonskiy@gmail.com>
2021-10-12 16:32:23 +0300

    changed psot policy

--- middleware/policy.middleware.js
+++ middleware/policy.middleware.js
@@ -1,12 +1,13 @@
 import Post from '../models/post.model.js';
+import {ApiError} from '../exceptions/api.js';
 
 export const postPolicy = async (req, res, next) => {
   try {
     const post = await Post.findById(req.params.id).populate('user_id');
 
     if (post.userId !== req.user._id)
-      return res.status(403)
-          .json({message: 'You don\'t have permission to delete the post.'});
+      return next(ApiError.permissionError(
+          'You don\'t have permission to delete the post.'));
 
     next();
   } catch (e) {
