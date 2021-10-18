51e0ed0f495906918aa2abddbf13a9707f440547
solonskiy <igorsolonskiy@gmail.com>
2021-10-18 13:36:23 +0300

    added webpack to seed

--- src/seeders/seed.js
+++ src/seeders/seed.js
@@ -1,20 +1,23 @@
 import mongoose from '../core/db';
 import {users} from './users';
-import {config} from 'dotenv';
 import {posts} from './posts';
-
-config();
+import env from '../env';
 
 (async () => {
   console.time('total time');
   console.time('connect DB');
 
+  await mongoose.connect(env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
   await mongoose.connection.db.dropDatabase();
   console.timeEnd('connect DB');
   console.time('seed runs');
 
-  const users = await users();
-  await posts(users);
+  const usersList = await users();
+  await posts(usersList);
 
   console.timeEnd('seed runs');
   console.timeEnd('total time');
